4dfdf2294d087595de23ff19b68bdeb69924cd9f
Matias Campos Aguirre <mat.campos.agui@gmail.com>
2025-03-18 00:18:46 -0300

    Primer commit - CRUD Spring Boot

---
+++ src/main/java/com/example/springboot_car_dealership/SpringbootCarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_car_dealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCarDealershipApplication.class, args);
+	}
+
+}
